54ce1c9550a816702e7f7b04609c42443441c3ec
Luisa Gil <luisamariagilgarcia24@gmail.com>
2021-06-27 16:35:30 -0500

    Se realizan ejercicios con Stepverifier y TestPushier

---
+++ src/main/java/com/example/TallerPruebasUnitariasBackend/TallerPruebasUnitariasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TallerPruebasUnitariasBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerPruebasUnitariasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerPruebasUnitariasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TallerPruebasUnitariasBackend/TallerPruebasUnitariasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TallerPruebasUnitariasBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerPruebasUnitariasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
